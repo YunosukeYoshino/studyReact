631eb58d12115d669faaec65edc0dfc78ee587e5
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2022-03-26 00:17:53 +0900

    ✏️ aboutの修正

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Footer } from '../components/Footer'
 import styles from '../styles/Home.module.css'
-
+import { Main } from '../components/main'
 export default function Home() {
   return (
     <div className={styles.container}>
